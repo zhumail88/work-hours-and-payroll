913affe125f1edb1d7b8e4120c49a41693629c72
Shumail <shumailtassadaq3@gmail.com>
2026-01-17 15:56:31 +0500

    critical: moved the firebase api keys to an env file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNeO0LeKdwXW0Xl2UrV5QGsKxX5Ik3GLg",
-  authDomain: "work-hours-and-payroll.firebaseapp.com",
-  projectId: "work-hours-and-payroll",
-  storageBucket: "work-hours-and-payroll.firebasestorage.app",
-  messagingSenderId: "906601955088",
-  appId: "1:906601955088:web:776d3e76c431e010d6af95",
-  measurementId: "G-24QMCG841Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
